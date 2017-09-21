52858d1a2a8abc373725661703ddaf1c6767b32d
christian-edgardo-rodriguez <christian.rodriguez@unitec.edu>
2017-09-21 13:34:31 -0600

    localhost corregido

--- services/posible.js
+++ services/posible.js
@@ -3,7 +3,7 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
-var url='http://localhost:8000/';
+var url='https://examen2-backend-ux.herokuapp.com/';
 export default{
 	buscarPosibles(){
 		return Vue.http.get(url+'posibles');
